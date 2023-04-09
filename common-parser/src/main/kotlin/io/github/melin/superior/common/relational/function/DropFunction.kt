04ffb669f35f3b44fe9d7dbc7e438c3fe5601039
melin <libinsong1204@gmail.com>
2023-04-09 10:58:25 +0800

    update

--- common-parser/src/main/kotlin/io/github/melin/superior/common/relational/function/DropFunction.kt
+++ common-parser/src/main/kotlin/io/github/melin/superior/common/relational/function/DropFunction.kt
@@ -3,12 +3,9 @@ package io.github.melin.superior.common.relational.function
 import io.github.melin.superior.common.PrivilegeType
 import io.github.melin.superior.common.Statement
 
-data class CreateFunction(
+data class DropFunction(
     val schemaName: String?,
-    val funcName: String,
-    var temporary: Boolean = false,
-    val className: String?,
-    val file: String?
+    val funcName: String
 ) : Statement() {
-    override val privilegeType: PrivilegeType = PrivilegeType.CREATE
+    override val privilegeType: PrivilegeType = PrivilegeType.DROP
 }
