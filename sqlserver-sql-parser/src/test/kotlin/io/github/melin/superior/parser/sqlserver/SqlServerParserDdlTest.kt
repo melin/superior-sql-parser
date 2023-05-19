9175cd35f05283c18d9d409d9939fbe60119e866
melin <libinsong1204@gmail.com>
2023-05-19 14:09:17 +0800

    sql server

---
+++ sqlserver-sql-parser/src/test/kotlin/io/github/melin/superior/parser/sqlserver/SqlServerParserDdlTest.kt
@@ -0,0 +1,8 @@
+package io.github.melin.superior.parser.sqlserver
+
+/**
+ * Created by libinsong on 2020/6/30 11:05 上午
+ */
+class SqlServerParserDdlTest {
+    
+}
--- sqlserver-sql-parser/src/test/kotlin/io/github/melin/superior/parser/sqlserver/SqlServerParserDmlTest.kt
+++ sqlserver-sql-parser/src/test/kotlin/io/github/melin/superior/parser/sqlserver/SqlServerParserDmlTest.kt
@@ -25,4 +25,6 @@ class SqlServerParserDmlTest {
             Assert.fail()
         }
     }
+
+
 }
